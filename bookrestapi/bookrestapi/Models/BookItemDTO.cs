821755f876ccef03d327fa977da972e41ff142ae
Minna Pelkonen <minnap@gmail.com>
2022-03-16 18:44:33 +0200

    Limit acceptable BookItem string lengths

--- bookrestapi/bookrestapi/Models/BookItemDTO.cs
+++ bookrestapi/bookrestapi/Models/BookItemDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace bookrestapi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bookrestapi.Models
 {
     public class BookItemDTO
     {
         public long Id { get; set; }
+        [StringLength(200)]
         public string? Title { get; set; }
+        [StringLength(200)]
         public string? Author { get; set; }
+        [StringLength(5000)]
         public string? Description { get; set; }
     }
 }
